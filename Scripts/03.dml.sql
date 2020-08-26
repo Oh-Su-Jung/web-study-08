3dc82bd4b151f0c6fd67f12a1c643c37bfd106ea
sujung <tnwjddl1996@gmail.com>
2020-08-26 10:59:57 +0900

    database 작업완료

---
+++ Scripts/03.dml.sql
@@ -0,0 +1,7 @@
+INSERT ALL
+	INTO MEMBER values('ÀÌ¼Ò¹Ì', 'somi', '1234', 'gmd@naver.com', '010-2362-5157', 0)
+	INTO MEMBER values('ÇÏ»ó¿À', 'sang12', '1234', 'ha12@naver.com', '010-5629-8888', 1)
+	INTO MEMBER values('±èÀ±½Â', 'light', '1234', 'youn1004@naver.com', '010-9999-8282', 0)
+	SELECT 1 FROM DUAL;
+	
+SELECT * FROM MEMBER;
